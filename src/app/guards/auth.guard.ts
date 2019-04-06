c9cd48224fc9536f4e55f9decc084aa673ee7407
TsionAlexandr <tsick83@gmail.com>
2019-04-06 03:37:56 +0300

    first commit

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { GlobalAuthService } from '../services/global-auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private globalAuthService: GlobalAuthService,
+    private router: Router
+  ) {
+  }
+
+  canActivate() {
+    if (!this.globalAuthService.isLogin) {
+      this.router.navigate(['/auth/login']);
+      return false;
+    } else {
+        return true;
+    }   
+  }
+}
